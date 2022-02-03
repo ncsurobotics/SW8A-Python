86a2c55d07d8a57ca498cd1516547a0cd767f90a
Bennett Petzold <bennettpetzold01@gmail.com>
2022-02-02 20:02:46 -0500

    Add documentation and improve style

--- Object Orientation/acousticsObjOri.py
+++ Object Orientation/acousticsObjOri.py
@@ -14,23 +14,37 @@ class Acoustics:
         self.delta_z = delta_z
         self.delta_t = 0;
 
-        # Device attributes
+        # Device variables
         self.chandle = ctypes.c_int16()
         self.status = {}
-        self.COUPLING_TYPE = 1 # PS4000a_DC
-        self.RANGE = 7 # PS4000a_2V
-        self.ANALOG_OFFSET = 0 # in volts
+        self.time_interval_ns = ctypes.c_float()
+        self.returned_max_samples = ctypes.c_int32()
+        self.oversample = ctypes.c_int16(1)
+
+        # Run attributes
         self.PRE_TRIGGER_SAMPLES = 20000
         self.POST_TRIGGER_SAMPLES = 60000
         self.MAX_SAMPLES = self.PRE_TRIGGER_SAMPLES + self.POST_TRIGGER_SAMPLES
         self.TIMEBASE = 64 # No idea what period this is
-        self.time_interval_ns = ctypes.c_float()
-        self.returned_max_samples = ctypes.c_int32()
-        self.oversample = ctypes.c_int16(1)
         self.TIME_INDISPOSED = None # milliseconds
         self.SEGMENT_INDEX = 0
         self.LP_READY = None # uses ps4000aIsReady, not ps4000aBlockReady
         self.P_PARAMETER = None
+
+        # Channel attributes
+        self.COUPLING_TYPE = 1 # PS4000a_DC
+        self.RANGE = 7 # PS4000a_2V
+        self.ANALOG_OFFSET = 0 # in volts
+        # Channel/Trigger attributes
+        self.ENABLED = 1
+
+        # Trigger attributes
+        self.THRESHOLD = 8200 # ADC counts
+        self.DIRECTION = 2 # PS4000a_RISING
+        self.DELAY = 0 # seconds
+        self.AUTO_TRIGGER = 0 # milliseconds
+
+        # Buffer Attributes
         self.MODE = 0 # PS4000A_RATIO_MODE_NONE
         self.START_INDEX = 0
         self.DOWNSAMPLE_RATIO = 0
@@ -40,10 +54,8 @@ class Acoustics:
         self.adc_2mV_maxes = []
 
     def initialize(self):
-        '''
-            Find a way to write this in such a way that it is a loop instead of brute.
-            Potentially allows code to be somewhat reusable under more hydrophones
-        '''
+        '''Sets up the physical PicoScope interface.'''
+
         self.status["openunit"] = ps.ps4000aOpenUnit(ctypes.byref(self.chandle), None)
         try:
             assert_pico_ok(self.status["openunit"])
@@ -59,32 +71,30 @@ class Acoustics:
         self.set_trigger(0)
 
     def open_channels(self):
-        ENABLED = 1
+        '''Creates a connection for each channel.
+
+        Goes from 0 to one less than channels size.'''
+
         i = self.channels.__len__()
         while i > 0:
             self.status["setCh" + str(i)] = \
-                ps.ps4000aSetChannel(self.chandle, i, ENABLED, \
+                ps.ps4000aSetChannel(self.chandle, i, self.ENABLED, \
                 self.COUPLING_TYPE, self.RANGE, self.ANALOG_OFFSET)
             i -= 1
 
     def set_trigger(self, i):
-        ENABLED = 1
-        THRESHOLD = 8200 # ADC counts
-        DIRECTION = 2 # PS4000a_RISING
-        DELAY = 0 # seconds
-        AUTO_TRIGGER = 0 # milliseconds
-        self.status["trigger"] = ps.ps4000aSetSimpleTrigger(self.chandle, ENABLED, \
-                i, THRESHOLD, DIRECTION, DELAY, AUTO_TRIGGER)
+        '''Sets a sampling threshold.
+
+        Parameters:
+            i: The channel number to watch
+        '''
+
+        self.status["trigger"] = ps.ps4000aSetSimpleTrigger(self.chandle, self.ENABLED, \
+                i, self.THRESHOLD, self.DIRECTION, self.DELAY, self.AUTO_TRIGGER)
         assert_pico_ok(self.status["trigger"])
 
-    def data_sample(self):
-        '''
-            Find a way to write this in such a way that it is a loop instead of brute.
-            Potentially allows code to be somewhat reusable under more hydrophones
-        '''
-        return 0
 
-    def time_difference(channel_one, channel_two):
+    def time_difference(self, channel_one, channel_two):
         '''
             Convolutional function to determine the delta t value based on cross correlation
         '''
@@ -107,31 +117,36 @@ class Acoustics:
         return cross_correlation.index(max(cross_correlation))
 
     def run(self):
+        '''Collects samples from the PicoScope.'''
+
         # Timebase information
         self.status["getTimebase2"] = ps.ps4000aGetTimebase2(self.chandle, self.TIMEBASE, \
                 self.MAX_SAMPLES, ctypes.byref(self.time_interval_ns), \
                 ctypes.byref(self.returned_max_samples), 0)
         assert_pico_ok(self.status["getTimebase2"])
+
         # Block capture
         self.status["runBlock"] = ps.ps4000aRunBlock(self.chandle, self.PRE_TRIGGER_SAMPLES, \
                 self.POST_TRIGGER_SAMPLES, self.TIMEBASE, self.TIME_INDISPOSED, \
                 self.SEGMENT_INDEX, self.LP_READY, self.P_PARAMETER)
         assert_pico_ok(self.status["runBlock"])
+
         # Wait for data collection to finish
         ready = ctypes.c_int16(0)
         check = ctypes.c_int16(0)
         while ready.value == check.value:
             self.status["isReady"] = ps.ps4000aIsReady(self.chandle, ctypes.byref(ready))
+
         self.buffers()
-        # create overflow location
-        overflow = ctypes.c_int16()
-        # create converted type maxSamples
-        c_MAX_SAMPLES = ctypes.c_int32(self.MAX_SAMPLES)
+        overflow = ctypes.c_int16() # create overflow location
+        C_MAX_SAMPLES = ctypes.c_int32(self.MAX_SAMPLES) # create converted type maxSamples
+
         # Retrieve buffer data
         self.status["getValues"] = ps.ps4000aGetValues(self.chandle, self.START_INDEX, \
-                ctypes.byref(c_MAX_SAMPLES), self.DOWNSAMPLE_RATIO, self.DOWNSAMPLE_RATIO_MODE, \
+                ctypes.byref(C_MAX_SAMPLES), self.DOWNSAMPLE_RATIO, self.DOWNSAMPLE_RATIO_MODE, \
                 0, ctypes.byref(overflow))
         assert_pico_ok(self.status["getValues"])
+
         # ADC counts to mV
         i = self.channels.__len__()
         while i < 0:
@@ -139,6 +154,8 @@ class Acoustics:
             i -= 1
 
     def buffers(self):
+        '''Creates buffers to capture data.'''
+
         buffer_min = (ctypes.c_int16 * self.MAX_SAMPLES)()
         i = self.channels.__len__()
         while i > 0:
@@ -147,7 +164,7 @@ class Acoustics:
             assert_pico_ok(self.status["setDataBuffers" + str(i)])
             i -= 1
 
-    def c_val(delta_a, delta_t):
+    def c_val(self, delta_a, delta_t):
         d = delta_t * 1480
         c = (d**2) / ( (delta_a**2) - d**2)
 
